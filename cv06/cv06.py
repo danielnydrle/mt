256c9d1320dfb5ae70e8cf5c9052db9b7abbd3ee
Daniel Nýdrle <nydrledaniel@gmail.com>
2024-10-28 23:41:35 +0100

    cv06 huffman

--- cv06/cv06.py
+++ cv06/cv06.py
@@ -1,3 +1,5 @@
+import heapq
+
 class RLE:
     """
     RLE enkoder/dekoder
@@ -33,14 +35,21 @@ class RLE:
                 counter_buffer.append(char)
             else:
                 decoded.append(f"{int(ord(char) - 33)}" * int(''.join(counter_buffer)))
-                counter_buffer = []
+                counter_buffer.clear()
         return ''.join(decoded)
 
-    def __str__(self):
-        return f"""
-        RLE kompresor
-        {self}
-        """
+class HuffmanNode:
+    """
+    Uzel Huffmanova stromu
+    """
+    def __init__(self, symbol, freq):
+        self.symbol = symbol
+        self.freq = freq
+        self.left = None
+        self.right = None
+
+    def __lt__(self, other):
+        return self.freq < other.freq
 
 class Huffman:
     """
@@ -48,15 +57,107 @@ class Huffman:
     """
     def __init__(self, dictionary: str):
         self.dict = dictionary
+        self._tree = None
+        self._codes = {}
+
+    def _build_tree(self, data: str):
+        """
+        Vytvoreni Huffmanova stromu
+        """
+        freqs = {}
+        for char in data:
+            freqs[char] = freqs.get(char, 0) + 1
+        heap = [HuffmanNode(symbol, freq) for symbol, freq in freqs.items()]
+        heapq.heapify(heap) # min-halda
+
+        while len(heap) > 1:
+            left = heapq.heappop(heap)
+            right = heapq.heappop(heap)
+            node = HuffmanNode(None, left.freq + right.freq)
+            node.left = left
+            node.right = right
+            heapq.heappush(heap, node)
+
+        self._tree = heap[0]
+
+        return heap[0]
+    
+    def _generate_codes(self, root: HuffmanNode, code: str = ""):
+        """
+        Generovani Huffmanovych kodu
+        """
+        if root is None:
+            return
+        
+        if root.symbol is not None:
+            self._codes[root.symbol] = code
+            return
+        
+        self._generate_codes(root.left, code + "0")
+        self._generate_codes(root.right, code + "1")
+
+    def _serialize_tree(self):
+        """
+        Serializace Huffmanova stromu
+        """
+        def _serialize(node: HuffmanNode):
+            if node is None:
+                return ""
+            if node.symbol is not None:
+                return f"1{node.symbol}"
+            return f"0{_serialize(node.left)}{_serialize(node.right)}"
+        return _serialize(self._tree)
+    
+    def _deserialize_tree(self, data: str):
+        """
+        Deserializace Huffmanova stromu
+        """
+        if not data:
+            return None, ""
+        
+        if data[0] == "1":
+            return HuffmanNode(data[1], 0), data[2:]
+        
+        node = HuffmanNode(None, 0)
+        left, data = self._deserialize_tree(data[1:])
+        right, data = self._deserialize_tree(data)
+        node.left = left
+        node.right = right
+        return node, data
+            
 
     def encode(self, data: str):
-        pass
+        """
+        Huffmanovo kodovani
+        """
+        if not data:
+            return ""
+        data = "".join(str(char) for char in data)
+        self._build_tree(data)
+        self._generate_codes(self._tree)
+        serialized_tree = self._serialize_tree()
+        encoded = "".join([self._codes[str(char)] for char in data])
+        return f"{serialized_tree}|{encoded}" # oddeleni stromu a zakodovanych dat
 
     def decode(self, compressed_data: str):
-        pass
-
-    def __str__(self):
-        return ""
+        """
+        Huffmanovo dekodovani
+        """
+        if not compressed_data:
+            return ""
+        serialized_tree, encoded = compressed_data.split("|")
+        self._tree, _ = self._deserialize_tree(serialized_tree)
+        decoded = []
+        node = self._tree
+        for char in encoded:
+            if char == "0":
+                node = node.left
+            else:
+                node = node.right
+            if node.symbol is not None:
+                decoded.append(node.symbol)
+                node = self._tree
+        return "".join(decoded)
     
 def load_file(file_path):
     """
@@ -87,9 +188,22 @@ if __name__ == "__main__":
     print_delimiter()
 
     DECODED_RLE = rle.decode(ENCODED_RLE)
-    print(f"RLE dekodovana data: {DECODED_RLE}")
+    print(f"Dekodovana data RLE: {DECODED_RLE}")
     print_delimiter()
 
     print(INPUT == DECODED_RLE)
+    print("\n\n")
 
     print("=== Huffman ===")
+
+    huff = Huffman(SYMBOL_DICT)
+
+    ENCODED_HUFF = huff.encode(INPUT)
+    print(f"Zakodovana data Huffman: {ENCODED_HUFF}")
+    print_delimiter()
+
+    DECODED_HUFF = huff.decode(ENCODED_HUFF)
+    print(f"Dekodovana data Huffman: {DECODED_HUFF}")
+    print_delimiter()
+
+    print(INPUT == DECODED_HUFF)
